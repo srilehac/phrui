4594e47e3b316d3e16f1e97f549e701e75967037
rahul <rahul.desai@rapidqube.com>
2017-12-14 14:51:30 +0530

    image changed

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,5 +1,5 @@
     export default {
-    base_url: 'http://192.168.0.26:8000', // local1
- //   base_url: 'http://119.81.59.59:8000',//softlayer
+  //  base_url: 'http://192.168.0.26:8000', // local1
+    base_url: 'http://119.81.59.59:8000',//softlayer
    // base_url: 'http://192.168.11.222:8080' // local2
 }
